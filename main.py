958372d7529f7d52b9bf00f55cff1a0fcccb08b1
Aleksey K [koshi8bit] <koshi8bit@mail.ru>
2021-05-06 15:23:54 +0700

    .gitignore updated

--- main.py
+++ main.py
@@ -8,12 +8,11 @@ def i_am_alive():
     return "I am alive 80!"
 
 
-@app.route('/t', methods=['GET'])
+@app.route('/foo', methods=['GET'])
 def i_am_alive2():
-    return "sam tyty!"
+    return "bar"
 
 
 if __name__ == '__main__':
     # app.run(debug=True, port=8080)
     app.run(debug=True, port=80)
-    
